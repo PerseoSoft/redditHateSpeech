449ba3d8e3b722fc0023c1e9b76e4aad7e72da2c
juan <jbarsce@gmail.com>
2021-11-12 16:09:45 -0300

    función de clustering de ipynb -> .py

---
+++ src/clustering_utils.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+
+def vectorize(list_of_docs, model):
+    """Generate vectors for list of documents using a Word Embedding
+
+    Args:
+        list_of_docs: List of documents
+        model: Gensim's Word Embedding
+
+    Returns:
+        List of document vectors
+    """
+    features = []
+
+    for tokens in list_of_docs:
+        zero_vector = np.zeros(model.vector_size)
+        vectors = []
+        for token in tokens:
+            if token in model.wv:
+                try:
+                    vectors.append(model.wv[token])
+                except KeyError:
+                    continue
+        if vectors:
+            vectors = np.asarray(vectors)
+            avg_vec = vectors.mean(axis=0)
+            features.append(avg_vec)
+        else:
+            features.append(zero_vector)
+    return features
+
+
+def mbkmeans_clusters(
+    X, 
+    k, 
+    mb, 
+    print_silhouette_values, 
+):
+    """Generate clusters and print Silhouette metrics using MBKmeans
+
+    Args:
+        X: Matrix of features.
+        k: Number of clusters.
+        mb: Size of mini-batches.
+        print_silhouette_values: Print silhouette values per cluster.
+
+    Returns:
+        Trained clustering model and labels based on X.
+    """
+    km = MiniBatchKMeans(n_clusters=k, batch_size=mb).fit(X)
+    print(f"For n_clusters = {k}")
+    print(f"Silhouette coefficient: {silhouette_score(X, km.labels_):0.2f}")
+    print(f"Inertia:{km.inertia_}")
+
+    if print_silhouette_values:
+        sample_silhouette_values = silhouette_samples(X, km.labels_)
+        print(f"Silhouette values:")
+        silhouette_values = []
+        for i in range(k):
+            cluster_silhouette_values = sample_silhouette_values[km.labels_ == i]
+            silhouette_values.append(
+                (
+                    i,
+                    cluster_silhouette_values.shape[0],
+                    cluster_silhouette_values.mean(),
+                    cluster_silhouette_values.min(),
+                    cluster_silhouette_values.max(),
+                )
+            )
+        silhouette_values = sorted(
+            silhouette_values, key=lambda tup: tup[2], reverse=True
+        )
+        for s in silhouette_values:
+            print(
+                f"    Cluster {s[0]}: Size:{s[1]} | Avg:{s[2]:.2f} | Min:{s[3]:.2f} | Max: {s[4]:.2f}"
+            )
+    return km, km.labels_
